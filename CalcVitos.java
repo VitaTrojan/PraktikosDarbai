b38091c98a32795e74fb30c6c45baafbca186c6b
VitaTrojan <55578774+VitaTrojan@users.noreply.github.com>
2019-10-02 23:35:38 +0300

    Update CalcVitos.java
    perdaryta su switch

--- CalcVitos.java
+++ CalcVitos.java
@@ -6,53 +6,67 @@ public class CalcVitos {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double b;
         System.out.print("Iveskite pirmaji skaiciu: ");
         double a = input.nextDouble();
         System.out.print("Iveskite operacijos zenkla ( +, -, *, /, sqrt, sin): ");
         String zenklas = input.next();
-        if (zenklas.equals("+") | zenklas.equals("-") | zenklas.equals("*") | zenklas.equals("/") | zenklas.equals("^") | zenklas.equals("%")) {
-            System.out.print("Iveskite antraji skaiciu: ");
-             b = input.nextDouble();
-
-
-        if (zenklas.equals("+")) {
-            addition(a, b);
-        } else if (zenklas.equals("-")) {
-            minus(a, b);
-        } else if (zenklas.equals("*")) multiply(a, b);
-       else if (zenklas.equals("/")) {
-            if (b == 0) System.out.println("Dalyba is nulio negalima");
-        } else {
-            division(a, b);
-        }}
-    else if(zenklas.equals("sqrt")) {sqrt(a);}
-    else if(zenklas.equals("sin")){sin(a);}
-
-}
-
-
-    private static void addition(double a, double b) {
-        System.out.println("Suma yra: " + (a + b));
-
+        switch (zenklas) {
+            case "+":
+                addition(a);
+                break;
+            case "-":
+                minus(a);
+                break;
+            case "*":
+                multiply(a);
+                break;
+            case "/":
+                division(a);
+                break;
+            case "sqrt":
+                sqrt(a);
+                break;
+            case "sin":
+                sin(a);
+                break;
+            default:
+                System.out.println("Tokia operacija dar neaprašyta. Apgailestauju.");
+        }
     }
 
-    private static void minus(double a, double b) {
-        System.out.println("Skirtumas yra: " + (a - b));
+    private static void addition(double a) {
+        System.out.print("Iveskite antraji skaiciu: ");
+        double b = input.nextDouble();
+        System.out.println("Suma yra: " + String.format("%.4f", (a + b)));
     }
 
-    private static void multiply(double a, double b) {
-        System.out.println("Sandauga yra: " + (a * b));
+    private static void minus(double a) {
+        System.out.print("Iveskite antraji skaiciu: -");
+        double b = input.nextDouble();
+        System.out.println("Skirtumas yra: " + String.format("%.4f", (a - b)));
     }
 
-    private static void division(double a, double b) {
-        System.out.println("Dalmuo yra: " + (a / b));
+    private static void multiply(double a) {
+        System.out.print("Iveskite antraji skaiciu: *");
+        double b = input.nextDouble();
+        System.out.println("Sandauga yra: " + String.format("%.4f", (a * b)));
+    }
+
+    private static void division(double a) {
+        System.out.print("Iveskite antraji skaiciu: /");
+        double b = input.nextDouble();
+        System.out.println("Dalmuo yra: " + String.format("%.4f", (a / b)));
+
     }
 
     private static void sqrt(double a) {
-        System.out.println("Saknis yra: " + Math.sqrt(a));
+        System.out.println("Saknis yra: " + String.format("%.4f", (Math.sqrt(a))));
     }
+
     private static void sin(double a) {
-        System.out.println("Sin yra: " + Math.sin(a));
+        System.out.println("Sin yra: " + String.format("%.4f", (Math.sin(a))));
     }
 }
+	
+	
+	
