abb153409946ce567657ffaa7e6ee192bba42465
VitaTrojan <55578774+VitaTrojan@users.noreply.github.com>
2019-10-13 20:57:55 +0300

    visi

--- Nd10.java
+++ Nd10.java
@@ -8,11 +8,112 @@ public class Nd10 {
     Random random = new Random();
 
     public static void main(String[] args) {
-        sortedArray(); //task 1, create arrays of integer and sorted with Bubble sorting
+        //  sortedArray(); //task 1, create arrays of integer and sorted with Bubble sorting
         //fibonaciNumbers();//task nr 2, print 120 number of Fibonacci
         // calculateAverageOfTwoArrays();// task nr 3
 
+        //sortingStringArray (); //task nr4 -- nerusiuoja iki galo, kazkadaryti su antru for
+        longArraySorting();// task nr5
+
+
     }
+
+    private static void longArraySorting() {
+        long[] array = new long[5000];
+        fillArrayLong(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        quicksorting(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+    }
+
+    private static void quicksorting(long array[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+            quicksorting(array, begin, partitionIndex - 1);
+            quicksorting(array, partitionIndex + 1, end);
+
+        }
+
+
+    }
+
+    private static int partition(long array[], int begin, int end) {
+
+        long pivot = array[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                long swapTemp = array[i];
+                array[i] = array[j];
+                array[j] = swapTemp;
+
+            }
+        }
+        long swapTemp = array[i + 1];
+        array[i + 1] = array[end];
+        array[end] = swapTemp;
+        return i + 1;
+    }
+
+    private static void sortingStringArray() {
+        String[] stringArray = {"d", "vardas", "valgis", "preke", "c", "kede", "lova", "stalas", "kompiuteris", "Rude"};
+        // String [] arrays = new String [stringArray.length];
+        for (int i = 0; i < (stringArray.length - 1); i++) {
+
+            int pirmoIlgis = stringArray[i].length();
+            int antroIlgis = stringArray[i + 1].length();
+            int trumpesnis = Math.min(pirmoIlgis, antroIlgis);
+            for (int j = 1; j < stringArray.length - i; j++) {
+                for (int k = 0; k < trumpesnis; k++) {
+                    if (stringArray[i].charAt(k) == stringArray[i + 1].charAt(k)) {
+                        continue;
+                    } else if (stringArray[i].charAt(k) < stringArray[i + 1].charAt(k)) {
+                        break;
+                    } else if (stringArray[i].charAt(k) > stringArray[i + 1].charAt(k)) {
+                        String temp = stringArray[i + 1];
+                        stringArray[i + 1] = stringArray[i];
+                        stringArray[i] = temp;
+                        System.out.println(Arrays.toString(stringArray));
+                        break;
+
+
+                    }
+                }
+                System.out.println(Arrays.toString(stringArray));
+            }
+        }
+        //for (int i=0, j=1; j<stringArray.length; i++, j++){
+        //   int iLength = stringArray[i].length();
+        // int iPlus = stringArray[j].length();
+        //    int shorter = 0;
+        //  if (iLength > iPlus) {
+        //    shorter = iPlus;
+        //} else {
+        //  shorter = iLength;
+        //}
+        //for (int k = 0; k<shorter; k++){
+        // int firstElement = stringArray[i].toLowerCase().charAt(k); //array[i]
+        //int seconfElement = stringArray[j].toLowerCase().charAt(k);//array [j]
+        //if (firstElement<seconfElement){
+        //break;}
+        //else if (firstElement>seconfElement){
+        //  String temp=stringArray[j];
+        //stringArray[j]=stringArray[i];
+        //stringArray[i]=temp;
+        //System.out.println((Arrays.toString(stringArray)));
+        //}
+
+    }//
+    //}
+    //System.out.print(Arrays.toString(stringArray));
+
 
     private static void sortedArray() {
         int[] array = new int[100];
@@ -70,6 +171,16 @@ public class Nd10 {
         int[] numbers = new int[1000];
         for (int i = 0; i < 100; i++) {
             numbers[i] = random.nextInt(1000);
+        }
+        System.out.println(Arrays.toString(numbers));
+        return numbers;
+    }
+
+    private static long[] fillArrayLong(long[] array) {
+        Random random = new Random();
+        long[] numbers = new long[5000];
+        for (int i = 0; i < 5000; i++) {
+            numbers[i] = random.nextLong();
         }
         System.out.println(Arrays.toString(numbers));
         return numbers;
